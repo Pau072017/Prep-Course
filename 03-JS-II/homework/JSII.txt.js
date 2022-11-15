4e41eec099f0d3dcbc8ddcfe0d290ae25351ae1f
Pau072017 <pauladaniela_r@icloud.com>
2022-11-15 00:34:36 -0300

    JSII.text

---
+++ 03-JS-II/homework/JSII.txt.js
@@ -0,0 +1,10 @@
+JSII.txt
+
+Bucle for = los bucles nos permiten realizar tareas repetitivas de manera mas sencilla.
+el bucle for nos va a permitir repetir un bloque de codigo varias veces, se define la variable a utilizar para contar, desde
+donde se comienza a contar, tambien hasta donde se va a contar, y como se va a ir incrementando. 
+
+Operadores logicos = 
+&& significa y o and 
+|| significa o u or
+! significa not o no, niega lo que continua.
